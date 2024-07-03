deeb6675aeb5e5e269eac485feaa9f2528722f7f
NatalieShif <natalia.shif@gmail.com>
2024-07-03 10:10:14 +0300

    Continue bulls-and-cows (react)

--- react/bulls-and-cows/src/App.jsx
+++ react/bulls-and-cows/src/App.jsx
@@ -1,19 +1,22 @@
+import { useState } from 'react';
+
 import Header from './components/header/Header';
+
 
 function App() {
   // MISSION: convert "users" into "state" and make
   // this array to be their initial value
-
-  const users = [
-    { id: 77, fullName: 'Harleen Frances Quinzel', nick: 'Harley Quinn',
-      email:'harley@gmail.com', phone: '555-5555', gender: 'f',role:'player'},
-    { id: 121, fullName: 'Joakin Phoenix', nick: 'Joker',
-      email:'joker@gmail.com',  gender: 'm',role:'player'},
-    { id: 123, fullName: 'Bruce Wayne', nick: 'Batman',
-      email:'bat@gmail.com', phone: '555-5557',role:'player'},
-    { id: 111, fullName: 'Princess Diana of Thymiscira', nick: 'Wonder Woman',
-        email:'gal.gadot@gmail.com', phone: '555-5558', role:'admin'}
-  ];
+  const [users,setUsers] = useState(
+      [
+        { id: 77, fullName: 'Harleen Frances Quinzel', nick: 'Harley Quinn',
+          email:'harley@gmail.com', phone: '555-5555', gender: 'f',role:'player'},
+        { id: 121, fullName: 'Joakin Phoenix', nick: 'Joker',
+          email:'joker@gmail.com',  gender: 'm',role:'player'},
+        { id: 123, fullName: 'Bruce Wayne', nick: 'Batman',
+          email:'bat@gmail.com', phone: '555-5557',role:'player'},
+        { id: 111, fullName: 'Princess Diana of Thymiscira', nick: 'Wonder Woman',
+            email:'gal.gadot@gmail.com', phone: '555-5558', role:'admin'}
+      ]);
 
   return (
     <div className="App">
---
+++ react/bulls-and-cows/src/components/userList/UserList.jsx
@@ -0,0 +1,18 @@
+import './UserList.scss';
+import UserCard from '../userCard/UserCard';
+
+export default function UserList({users}) {
+  return (
+    // col-12 - 12 columns for the narowest screen (xs - extra small)
+    // col-sm-6 - starting from the small screen - take the half of bootstrap
+    <div className="col-12 col-sm-6 col-md-7">
+        <ul>
+{/* MISSION: create expression that if there are some users,
+creates "map" of <UserCard user={user} /> for each user,
+and if not, shows 
+<p className="display-5">This is a sociopatic game: no users</p>
+*/}
+        </ul>
+    </div>
+  )
+}
